150f1f460f2da54b39c253b6362ff0abba18835c
Oliver Radini <orr2@live.com>
2017-01-14 18:19:02 +0000

    guild component now correctly loads from fake guild data

--- herald/src/data/generate-guild-data.js
+++ herald/src/data/generate-guild-data.js
@@ -4,30 +4,49 @@ const fs = require('fs');
 
 const GUILD_COUNT= process.argv[2] || 1;
 
-let names = [
-    'MYTH',
-    'Dragon dragons',
-    'Kings of danger',
-    '%20maniacs',
-    '2aq123',
-    '123123',
-    'fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff fluff',
-    'undefined',
-    'false',
-]
+function getRandomArrayItem(arrayIn){
+    return arrayIn[Math.floor(Math.random() * arrayIn.length)];
+}
+
+function getGuildName(){
+    return getRandomArrayItem(namesFirsts) + ' ' + getRandomArrayItem(namesSeconds);
+}
+
+let namesFirsts = [
+    'Mythical',
+    'Iron',
+    'Knights of',
+    'Red',
+    'Heroes of',
+    'Kings of',
+    'Doctors of',
+    'Emerald',
+];
+
+let namesSeconds = [
+    'Knights',
+    'Dragons',
+    'Sheep',
+    'Funk',
+    'Dogs',
+    'Wolves',
+];
 
 let guilds = [];
+let guildNames = [];
 
 for (let i=0; i<GUILD_COUNT; i++){
     
-    let guildName = names[Math.floor(Math.random()*names.length)];
-    let guildHouse = Math.random() > 0.4 ? "None" : `Lot ${Math.floor(Math.random() * 150)+1}`
+    let guildName = getGuildName();
+    let guildHouse = Math.random() > 0.4 ? "None" : `Lot ${Math.floor(Math.random() * 150)+1}`;
     let guildRealmPoints = Math.floor(Math.random() * 100000000000); //TODO: is this right?
-    let guildRealm = ['Albion', 'Hibernia', 'Midgard'][Math.floor(Math.random()*3)]
+    let guildRealm = ['Albion', 'Hibernia', 'Midgard'][Math.floor(Math.random()*3)];
     let guildWebsite = '';
     let guildContact = '';
 
     let guildPlayers = [];//TODO: add this
+
+    guildNames.push(guildName);
 
     guilds.push({
         name: guildName,
@@ -39,17 +58,15 @@ for (let i=0; i<GUILD_COUNT; i++){
     });
 }
 
-console.dir(guilds);
+//write each guild to a file
+for (let i=0; i<guilds.length; i++){
+    let currGuild = guilds[i];
+    fs.writeFile(`./guilds/${currGuild.name.replace(/ /g, '-')}.json`, JSON.stringify(currGuild), function(err){
+        if (err) console.log(err); 
+    });
+}
 
-fs.writeFile('./guild-data.json', JSON.stringify(guilds));
-
-
-//constructor(public name: string,
-            //public contact: string,
-            //public website: string,
-            //public guildHouse: string,
-            //public guildRealmPoints: number,
-            //public players: string[],
-            //public realm: string){
-    //}
-//}
+//write the file for all guild names
+fs.writeFile(`./guilds.json`, JSON.stringify(guildNames), function(err){
+    if (err) console.log(err);
+});
